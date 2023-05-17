d8f99ed5f21e312be6d63d5d7671487753b87055
Shlomi Toussia-Cohen <shlomito@monday.com>
2023-05-17 09:47:52 +0300

    add storybook with some code example

--- src/App.tsx
+++ src/App.tsx
@@ -11,121 +11,16 @@ import {
   Modal,
   ModalFooter,
   Button,
+  Dropdown,
 } from "monday-ui-react-core";
 
 import { MoveArrowRight } from "monday-ui-react-core/icons";
 
-/**
- * What do you get from this exercise in Vibe
- * 1. Foundations - typography, colors, spacing
- * 2. Base components - Modal, Avatar (and group), Link, RadioButton
- * 3. Layout components - Flex
- * 4. Assets - Icon
- * 5. Storybook Docs
- * 6. Components types
- *
- * Figma tips
- * 1. Inspector
- * 2. Components Tree
- * 3. Measure spaces between items
- * 4. Links to Storybook
- *
- * 
- * Modal
- * ===========
- * What to use
- * ------------
- * Modal, Modal Content, Modal Footer
- * 
- * Issues
- * ------
- * 1. close button tooltip is broken
- * 2. title is mandatory but type says optional
- * 
- *
- * Description Text
- * ===========
- * What to use
- * ------------
- * CSS Variables
- * 
- * Issues
- * ------
- * 1. No component, must use scss mixin.
- * 2. no storybook documentation for the scss mixins
- * 3. scss mixins names are not the same as the figma one
- * 
- * AvatarGroup
- * ===========
- *    <AvatarGroup size={Avatar?.sizes?.SMALL} max={3}>
- *        {new Array(13).fill(" ").map((_, i) => (
- *          <Avatar
- *            key={i}
- *            type={Avatar?.types?.IMG}
- *            ariaLabel={`name-${i}`}
- *          />
- *        ))}
- *    </AvatarGroup>
- * Issues
- * ------
- * 1. Examples from storybook can't be used out-of-the-box because of images
- * 2. `src` should be mandatory
- * 3. component is partially broken if removing `ariaLabel`
- * 4. contains padding which is incorrect
- * 5. AvatarGroup doesn't have an enum property
- * 6. the +10 dropdown has css issue - spacing is overridden
- *
- * Flex
- * ======
- *
- * Issues
- * ------
- * 1. x-large (32px) gap size is missing
- * 2. gap is not documented
- * 3. default are not good - setting direction column sets alignment to center
- * 4. missing the option to render as a different html element tag (`form`)
- * 5. missing flex-item functionality
- *
- * Custom Section Box
- * ------------------
- * 1. spacing in figma is in numbers and not variables (16px vs. medium)
- *
- *
- * RadioButton
- * ===========
- *
- * Issues
- * ------
- * 1. Missing RadioButtonGroup
- * 2. must use `form` for `defaultCheck` to work
- * 3. RadioGroup is missing
- *
- * Icons
- * =====
- *
- * Issues
- * ------
- * 1. types are not exported. even when trying to use `typeVersions` for older ts verions - it can't find it
- * https://stackoverflow.com/questions/58990498/package-json-exports-field-not-working-with-typescript
- * 2. Icon component makes the component clickable. it should be only focusable
- *
- * Dropdown
- * ========
- *
- * Issues
- * ------
- * 1. the tags version isn't clear in the docs (says multiline, but its tags)
- * 2. types are broken
- *
- * Others
- * ======
- * 1. missing an InfoIcon component
- * 2. Figma doesn't display spacing tokens, only numbers
- */
+const dropdownOptions = [{value: 'Developer', label: 'Developer'}, {value: 'Designer', label: 'Designer'}];
 
 function App() {
   return (
-    <Modal show onClose={() => null} title="Board Permissions" width={'752px' as any}>
+    <Modal show onClose={() => null} title="Board Permissions" width={'something' as any} zIndex={0}>
       <ModalContent>
         <Flex
           gap={Flex.gaps?.LARGE}
@@ -176,7 +71,7 @@ function App() {
                     className={styles.extenedRadioItem}
                     gap={Flex?.gaps?.MEDIUM}
                   >
-                    <Flex style={{ flex: "1 0 auto" }} gap={Flex.gaps?.XS}>
+                    <Flex style={{width: '50%'}} gap={Flex.gaps?.XS}>
                       <Icon
                         iconType={Icon?.type?.SVG}
                         icon={MoveArrowRight}
@@ -186,9 +81,9 @@ function App() {
                         Select the specific columns of <b>items</b>
                       </span>
                     </Flex>
-                    <Flex style={{ flex: "1 0 auto" }}>
-                      <input style={{ width: "100%" }}></input>
-                    </Flex>
+                    <div style={{width: '50%'}}>
+                      <Dropdown multi defaultValue={dropdownOptions} options={dropdownOptions} insideOverflowContainer/>
+                    </div>
                   </Flex>
                 </Flex>
               </form>
---
+++ src/stories/Button.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Button as Test } from 'monday-ui-react-core';
+import 'monday-ui-react-core/dist/main.css';
+import './button.css';
+
+interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  return (
+    <Test
+      // type="button"
+      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </Test>
+  );
+};
